314d6f5aa3ffc06bd8ea397d895b37099245ad99
aimo0001 <aimo0001@stud.ek.dk>
2026-03-12 21:48:08 +0100

    initial spring boot project

---
+++ src/test/java/com/example/kinoxp/KinoXpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinoxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
