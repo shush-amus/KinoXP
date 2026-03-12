80adf5e64fd861685ea5cba21b543e3445a80c09
aimo0001 <aimo0001@stud.ek.dk>
2026-03-12 22:57:50 +0100

    Added Showing controller GET POST endpoints

---
+++ src/main/java/com/example/kinoxp/controller/ShowingController.java
@@ -0,0 +1,28 @@
+package com.example.kinoxp.controller;
+
+import com.example.kinoxp.model.Showing;
+import com.example.kinoxp.repository.ShowingRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/showings")
+public class ShowingController {
+
+    private final ShowingRepository showingRepository;
+
+    public ShowingController(ShowingRepository showingRepository) {
+        this.showingRepository = showingRepository;
+    }
+
+    @GetMapping
+    public List<Showing> getAllShowings() {
+        return showingRepository.findAll();
+    }
+
+    @PostMapping
+    public Showing createShowing(@RequestBody Showing showing) {
+        return showingRepository.save(showing);
+    }
+}
